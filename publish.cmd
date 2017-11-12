2cdf2af5a2d3600fc74a147e3116b594dd7ee8c7
Yura Horak <yhorak@blackthorn-vision.com>
2017-11-12 17:32:39 +0200

    initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=did-panas-Web-Deploy.pubxml -p:Password=zTdNdruyAvgRhw5hGW44ZbYcsqt7vMEaD037PG9ZmEdoJulBl54c1u5PnDaJ
+
